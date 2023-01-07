2d017c45183e2578c5e5ec9555ff110c709341d2
Phineas Lekgothoane <phineas@softidoc.co.za>
2023-01-07 14:01:27 +0200

    added overlay and display data when you visit a page

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -49,155 +49,7 @@
         </v-col>
 
         <v-col cols="12" md="9">
-          <v-container>
-            <v-row class="cardPanel">
-              <v-col class="py-0 px-0" cols="12" md="2">
-                <v-card
-                  class="cardVideo py-6"
-                  width="100%"
-                  height="100%"
-                  flat
-                  tile
-                  align="right"
-                >
-                  <div class="my-auto mx-2 hidden-md-and-up">
-                    <v-avatar color="#5692e8" class="button" size="36">
-                      <span class="white--text text-h5">9</span>
-                    </v-avatar>
-                  </div></v-card
-                >
-              </v-col>
-              <v-col class="py-0 px-0 d-flex" cols="12" md="10">
-                <v-card class="cardPanel text-left pa-2" width="100%" flat tile>
-                  <h3 class="textContent">Game Title</h3>
-                  <p class="textContent">Release Date: DD/MM/YYYY</p>
-                  <p class="textContent">
-                    Trysail transma durt sea Legs Trysail transma durt sea Legs
-                    Trysail transma durt sea Legs Trysail transma durt sea Legs
-                    Trysail transma durt sea Legs Trysail transma durt
-                  </p>
-                </v-card>
-                <div class="my-auto mx-2 hidden-sm-and-down">
-                  <v-avatar color="#5692e8" class="button" size="36">
-                    <span class="white--text text-h5">9</span>
-                  </v-avatar>
-                </div>
-              </v-col>
-            </v-row>
-          </v-container>
-
-          <v-container class="mt-4">
-            <v-row class="cardPanel">
-              <v-col class="py-0 px-0" cols="12" md="2">
-                <v-card
-                  class="cardVideo py-6"
-                  width="100%"
-                  height="100%"
-                  flat
-                  tile
-                  align="right"
-                >
-                  <div class="my-auto mx-2 hidden-md-and-up">
-                    <v-avatar color="#5692e8" class="button" size="36">
-                      <span class="white--text text-h5">7</span>
-                    </v-avatar>
-                  </div></v-card
-                >
-              </v-col>
-              <v-col class="py-0 px-0 d-flex" cols="12" md="10">
-                <v-card class="cardPanel text-left pa-2" width="100%" flat tile>
-                  <h3 class="textContent">Game Title</h3>
-                  <p class="textContent">Release Date: DD/MM/YYYY</p>
-                  <p class="textContent">
-                    Trysail transma durt sea Legs Trysail transma durt sea Legs
-                    Trysail transma durt sea Legs Trysail transma durt sea Legs
-                    Trysail transma durt sea Legs Trysail transma durt
-                  </p>
-                </v-card>
-                <div class="my-auto mx-2 hidden-sm-and-down">
-                  <v-avatar color="#5692e8" class="button" size="36">
-                    <span class="white--text text-h5">7</span>
-                  </v-avatar>
-                </div>
-              </v-col>
-            </v-row>
-          </v-container>
-
-          <v-container class="mt-4">
-            <v-row class="cardPanel">
-              <v-col class="py-0 px-0" cols="12" md="2">
-                <v-card
-                  class="cardVideo py-6"
-                  width="100%"
-                  height="100%"
-                  flat
-                  tile
-                  align="right"
-                >
-                  <div class="my-auto mx-2 hidden-md-and-up">
-                    <v-avatar color="#5692e8" class="button" size="36">
-                      <span class="white--text text-h5">5</span>
-                    </v-avatar>
-                  </div></v-card
-                >
-              </v-col>
-              <v-col class="py-0 px-0 d-flex" cols="12" md="10">
-                <v-card class="cardPanel text-left pa-2" width="100%" flat tile>
-                  <h3 class="textContent">Game Title</h3>
-                  <p class="textContent">Release Date: DD/MM/YYYY</p>
-                  <p class="textContent">
-                    Trysail transma durt sea Legs Trysail transma durt sea Legs
-                    Trysail transma durt sea Legs Trysail transma durt sea Legs
-                    Trysail transma durt sea Legs Trysail transma durt
-                  </p>
-                </v-card>
-                <div class="my-auto mx-2 hidden-sm-and-down">
-                  <v-avatar color="#5692e8" class="button" size="36">
-                    <span class="white--text text-h5">5</span>
-                  </v-avatar>
-                </div>
-              </v-col>
-            </v-row>
-          </v-container>
-
-          <v-container class="mt-4">
-            <v-row class="cardPanel">
-              <v-col class="py-0 px-0" cols="12" md="2">
-                <v-card
-                  class="cardVideo py-6"
-                  width="100%"
-                  height="100%"
-                  flat
-                  tile
-                  align="right"
-                >
-                  <div class="my-auto mx-2 hidden-md-and-up">
-                    <v-avatar color="#5692e8" class="button" size="36">
-                      <span class="white--text text-h5">2</span>
-                    </v-avatar>
-                  </div></v-card
-                >
-              </v-col>
-              <v-col class="py-0 px-0 d-flex" cols="12" md="10">
-                <v-card class="cardPanel text-left pa-2" width="100%" flat tile>
-                  <h3 class="textContent">Game Title</h3>
-                  <p class="textContent">Release Date: DD/MM/YYYY</p>
-                  <p class="textContent">
-                    Trysail transma durt sea Legs Trysail transma durt sea Legs
-                    Trysail transma durt sea Legs Trysail transma durt sea Legs
-                    Trysail transma durt sea Legs Trysail transma durt
-                  </p>
-                </v-card>
-                <div class="my-auto mx-2 hidden-sm-and-down">
-                  <v-avatar color="#5692e8" class="button" size="36">
-                    <span class="white--text text-h5">2</span>
-                  </v-avatar>
-                </div>
-              </v-col>
-            </v-row>
-          </v-container>
-
-          <v-container class="mt-4">
+          <v-container class="mt-4" v-for="post in posts" :key="post.id">
             <v-row class="cardPanel">
               <v-col class="py-0 px-0" cols="12" sm="2">
                 <v-card
@@ -217,17 +69,19 @@
               </v-col>
               <v-col class="py-0 px-0 d-flex" cols="12" sm="10">
                 <v-card class="cardPanel text-left pa-2" width="100%" flat tile>
-                  <h3 class="textContent">Game Title</h3>
-                  <p class="textContent">Release Date: DD/MM/YYYY</p>
+                  <h3 class="textContent">{{ post.name }}</h3>
                   <p class="textContent">
-                    Trysail transma durt sea Legs Trysail transma durt sea Legs
-                    Trysail transma durt sea Legs Trysail transma durt sea Legs
-                    Trysail transma durt sea Legs Trysail transma durt
+                    Release Date: {{ post.first_release_date }}
+                  </p>
+                  <p class="textContent">
+                    {{ post.summary }}
                   </p>
                 </v-card>
                 <div class="my-auto mx-2 hidden-sm-and-down">
-                  <v-avatar color="#5692e8" class="button" size="36">
-                    <span class="white--text text-h5">2</span>
+                  <v-avatar color="#5692e8" class="button" size="50">
+                    <span class="white--text text-h5">
+                      {{ post.rating }}
+                    </span>
                   </v-avatar>
                 </div>
               </v-col>
@@ -236,16 +90,41 @@
         </v-col>
       </v-row>
     </v-container>
+
+    <v-overlay :value="overlay" z-index="1000">
+      <v-progress-circular indeterminate size="64"></v-progress-circular>
+    </v-overlay>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "HelloWorld",
 
   data: () => ({
-    items: ["Foo", "Bar", "Fizz", "Buzz"],
+    posts: [],
+    overlay: false,
   }),
+
+  created() {
+    this.getVedioData();
+  },
+  methods: {
+    //calling vedio data Api
+
+    getVedioData() {
+      this.overlay = true;
+      axios
+        .get("https://public.connectnow.org.uk/applicant-test/")
+        .then((response) => {
+          this.posts = response.data;
+          console.log("data", response.data);
+          this.overlay = false;
+        })
+        .catch((error) => console.log(error));
+    },
+  },
 };
 </script>
 
